bec0cb4f70380e0c8b154634c5b2e135bdf2307a
lupinart <lupinart1008@gmail.com>
2025-09-13 02:04:10 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -231,7 +231,7 @@ const CONFIG = {
   ],
 
   // 博客文章区块
-  STARTER_BLOG_ENABLE: true, // 首页博文区块开关
+  STARTER_BLOG_ENABLE: false, // 首页博文区块开关
   STARTER_BLOG_TITLE: '設計筆記',
   STARTER_BLOG_COUNT: 3, // 首页博文区块默认展示前3篇文章
   STARTER_BLOG_TEXT_1: '最新分享',
