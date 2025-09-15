8e7e35003aa66ad056136b48a118bbff0c1af00f
lupinart <lupinart1008@gmail.com>
2025-09-15 10:15:32 +0800

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,5 @@
 export default function ErrorPage({ statusCode }) {
-  return <div>发生错误，状态码：{statusCode || 404}</div>
+  return <div>發生錯誤，：{statusCode || 404}</div>
 }
 ErrorPage.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
