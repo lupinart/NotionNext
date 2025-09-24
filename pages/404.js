ad3414557ab22d817117908f17dbae4672a5d640
lupinart <lupinart1008@gmail.com>
2025-09-24 15:42:49 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,7 @@ import BLOG from '@/blog.config'
 import { siteConfig } from '@/lib/config'
 import { getGlobalData } from '@/lib/db/getSiteData'
 import { DynamicLayout } from '@/themes/theme'
+import { useEffect } from 'react'
 
 /**
  * 404
@@ -9,19 +10,24 @@ import { DynamicLayout } from '@/themes/theme'
  * @returns
  */
 const NoFound = props => {
+  useEffect(() => {
+    console.log('404頁面加載')
+  }, [])
   const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
   return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
 }
 
-export async function getStaticProps({ locale }) {
+export async function getStaticProps(req) {
+  const { locale } = req
   const props = (await getGlobalData({ from: '404', locale })) || {}
   
-  // 為了確保 404 頁面載入速度快，可以考慮在這裡清空不必要的資料
-  // props.allPosts = []
-  
+  // 在 404 頁面不需要所有文章列表，只取必要的配置資料
+  delete props.posts
+  delete props.allPages
+
   return { 
     props,
-    revalidate: siteConfig('NEXT_REVALIDATE_SECOND', BLOG.NEXT_REVALIDATE_SECOND, props.NOTION_CONFIG)
+    revalidate: siteConfig('NEXT_REVALIDATE_SECOND', BLOG.NEXT_REVALIDATE_SECOND)
   }
 }
 
