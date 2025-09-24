1eb23abea90e9e143092639cfa7135e1de2e42a6
lupinart <lupinart1008@gmail.com>
2025-09-24 15:26:42 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -13,11 +13,16 @@ const NoFound = props => {
   return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
 }
 
-export async function getStaticProps(req) {
-  const { locale } = req
-
+export async function getStaticProps({ locale }) {
   const props = (await getGlobalData({ from: '404', locale })) || {}
-  return { props }
+  
+  // 為了確保 404 頁面載入速度快，可以考慮在這裡清空不必要的資料
+  // props.allPosts = []
+  
+  return { 
+    props,
+    revalidate: siteConfig('NEXT_REVALIDATE_SECOND', BLOG.NEXT_REVALIDATE_SECOND, props.NOTION_CONFIG)
+  }
 }
 
 export default NoFound
