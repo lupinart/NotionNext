9be6b158c68c2d20e9a39ac9382708a6292fc848
lupinart <lupinart1008@gmail.com>
2025-09-26 20:14:40 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -65,8 +65,8 @@ const CONFIG = {
   STARTER_ABOUT_TEXT:
     '嗨，我是 Lupin，一位插畫、平面與互動設計師。<br/><br/>我喜歡把情感轉化成畫面，透過插畫與設計，把故事說得有溫度也有力量。<br/>一路走來，我不只專注於 插畫 × 平面 × UI/UX，也在專案管理與團隊協作中找到更多設計的價值。<br/><br/>在這裡，我想紀錄自己與設計的對話，分享每一次嘗試與突破，讓設計不只是作品，而是一種能被感受與使用的體驗。<br/><br/>如果你也想透過設計傳達自己的價值，或許我們可以一起創造更多可能。',
   
-  STARTER_ABOUT_BUTTON_TEXT: '預約設計',
-  STARTER_ABOUT_BUTTON_URL: 'mailto:lupinart1008@gmail.com',
+  STARTER_ABOUT_BUTTON_TEXT: '瞭解更多',
+  STARTER_ABOUT_BUTTON_URL: 'https://www.lupinvisual.com/about',
   STARTER_ABOUT_IMAGE_1: '/images/starter/about/about-me.jpg',
   STARTER_ABOUT_IMAGE_2: '/images/starter/about/about_01.jpg',
   STARTER_ABOUT_TIPS_1: '',
